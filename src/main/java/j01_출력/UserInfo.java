86abf0f9adb90d7d81fcd8a1704fdf55cbeba555
rhehdgus2 <kdgkdg0217@naver.com>
2022-11-15 12:21:48 +0900

    챕터1 출력

---
+++ src/main/java/j01_출력/UserInfo.java
@@ -0,0 +1,14 @@
+package j01_출력;
+
+public class UserInfo {
+    public static void main(String[] args) {
+        String name = "고동현";
+        String address = "부산 부산진구 가야동";
+        String phone = "010-6784-6033";
+        int age = 26;
+
+        System.out.println(name + "의 주소는 " + address + "입니다.");
+        System.out.println(name + "의 연락처는 " + phone + "입니다.");
+        System.out.println(name + "의 나이는 " + age + "살입니다.");
+    }
+}
